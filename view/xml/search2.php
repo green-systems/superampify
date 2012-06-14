558c7b4435048bc533e1eb38d4ce61775d18b049
eskerda <eskerda@gmail.com>
2012-06-14 16:00:33 +0200

    Fix bug when no results

--- view/xml/search2.php
+++ view/xml/search2.php
@@ -22,9 +22,12 @@
 ?>
 <?php $data = $data['response']['searchResult2']; ?>
 <searchResult2>
+<?php if (isset($data['artist'])): ?>
 <?php foreach($data['artist'] as $artist): ?>
 <artist name="<?php echo $artist['name']; ?>" id="<?php echo $artist['id']; ?>"/>
 <?php endforeach; ?>
+<?php endif; ?>
+<?php if (isset($data['album'])): ?>
 <?php foreach($data['album'] as $album): ?>
 <album <?php foreach($album as $key=>$value): ?>
 <?php if (is_bool($value)){
@@ -38,6 +41,8 @@
 ?>
 <?php echo $key; ?>="<?php echo $value; ?>" <?php endforeach; ?> />
 <?php endforeach; ?>
+<?php endif; ?>
+<?php if (isset($data['song'])): ?>
 <?php foreach($data['song'] as $song): ?>
 <song <?php foreach($song as $key=>$value): ?>
 <?php if (is_bool($value)){
@@ -51,4 +56,5 @@
 ?>
 <?php echo $key; ?>="<?php echo $value; ?>" <?php endforeach; ?> />
 <?php endforeach; ?>
+<?php endif; ?>
 </searchResult2>
