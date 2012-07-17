dce12976973a922481e308188b6f3557a79d3be3
eskerda <eskerda@gmail.com>
2012-07-17 15:08:23 +0200

    Add user name as default music folder

--- app/view/xml/getMusicFolders.php
+++ app/view/xml/getMusicFolders.php
@@ -1,3 +1,5 @@
 <musicFolders>
-	<musicFolder id="0" name="Music"/>
+    <?php foreach($data['response']['musicFolders'] as $mFolder): ?>
+    <musicFolder id="<?php echo $mFolder['id']; ?>" name="<?php echo $mFolder['name']; ?>"/>
+    <?php endforeach; ?>
 </musicFolders>
